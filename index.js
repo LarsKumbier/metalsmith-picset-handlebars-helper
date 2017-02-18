54c195417f3e71c70ab8eb27a9e7a6404f992a6e
Anthony At Zinid <anthony@zinid.com>
2017-02-18 17:13:39 -0600

    Proof of concept metalsmith plugin functional

---
+++ index.js
@@ -0,0 +1,16 @@
+const _ = require('lodash')
+
+function plugin(opts) {
+	return function(files, metalsmith, done) {
+		console.log('Inside: metalsmith-picset-handlebars-helper')
+		console.log(opts)
+
+		_.forEach(files, (file, filename) => {
+			// console.log(filename)
+		})
+
+		setImmediate(done)
+	}
+}
+
+module.exports = plugin
