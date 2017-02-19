52e69bc1163f3aca967e4845d3ece4bf75b44501
Anthony At Zinid <anthony@zinid.com>
2017-02-18 18:35:42 -0600

    Noise reduction for pair plugin dev

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const _ = require('lodash')
 
 function plugin(opts) {
 	return function(files, metalsmith, done) {
+		/*
 		console.log('Inside: metalsmith-picset-handlebars-helper')
 		console.log(opts)
+		*/
 
 		_.forEach(files, (file, filename) => {
 			// console.log(filename)
